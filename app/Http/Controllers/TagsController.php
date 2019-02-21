41ea2290c51684e206177822333dba830c97fe86
nestorovicevo <nestorovicevo@gmail.com>
2019-02-21 21:18:56 +0100

    E sada stvarno radi!

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -9,7 +9,7 @@ class TagsController extends Controller
 {
     public function index(Tag $tag) {
 
-        $posts = $tag->posts-paginate(15);   ////morali smo da dodamo paginaciju, jer smo je prethodno imali, pa da nam ne bi izbacivala gresku
+        $posts = $tag->posts()->paginate(15);   ////morali smo da dodamo paginaciju, jer smo je prethodno imali, pa da nam ne bi izbacivala gresku
 
         return view('posts.index', compact('posts'));
     }
