dabe0a1418b27ab018c7f2fec37f18bb85987f02
nestorovicevo <nestorovicevo@gmail.com>
2019-02-21 21:53:34 +0100

    Zavrsiti kreiranje postova sa tagovima, nesto niej dobro

--- app/Post.php
+++ app/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
 
     const STORE_RULES = [
         'title' => 'required',
-        'body' => 'required | min:15'
+        'body' => 'required | min:15',
     ];
 
     public function comments()
